ac81bd720f87ddd2dc7c80008683995533b6f227
Sij-dev <sijeshp@gmail.com>
2023-04-23 10:12:43 +0400

    added exception block for api key failure

--- conscious_bot.py
+++ conscious_bot.py
@@ -21,46 +21,48 @@ def get_api_key():
 
 openai_api_key = get_api_key() 
 
+if len(openai_api_key) <= 25:
+    st.error("give a valid Open API key to proceed")
+else:    
+
 # model = st.selectbox(
 #     "Select a model",
 #     ("gpt-3.5-turbo (default)", "gpt-4 (make sure you have access to this model)","text-davinci-003","text-ada-001")
 # )
 
-## pinecone init
-index_name = "consciousness-openai"
-pincone_index = openai_util.init_pinecone(index_name)
-#chain, pineconeDocSearchHandler = openai_util.init_openAI_embedding_from_pinecone(pincone_index)
+    ## pinecone init
+    index_name = "consciousness-openai"
+    pincone_index = openai_util.init_pinecone(index_name)
+    #chain, pineconeDocSearchHandler = openai_util.init_openAI_embedding_from_pinecone(pincone_index)
 
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-    
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+        
 
-query = st.text_input("Query: ", key="input")
+    query = st.text_input("Query: ", key="input")
 
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = openai_util.get_initial_message()
-    
-if len(openai_api_key) <= 10:
-    st.error("give a valid Open API key to proceed")
-else:    
-    if query:
-        with st.spinner("generating..."):
-            messages = st.session_state['messages']
-            messages = openai_util.update_chat(messages, "user", query)
-            response = openai_util.openAI_get_response(pincone_index,query,openai_api_key)
-            
-            #response = openai_util.get_chatgpt_response(messages,model)
-            messages = openai_util.update_chat(messages, "assistant", response)
-            st.session_state.past.append(query)
-            st.session_state.generated.append(response)
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = openai_util.get_initial_message()
+        
 
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
+        if query:
+            with st.spinner("generating..."):
+                messages = st.session_state['messages']
+                messages = openai_util.update_chat(messages, "user", query)
+                response = openai_util.openAI_get_response(pincone_index,query,openai_api_key)
+                
+                #response = openai_util.get_chatgpt_response(messages,model)
+                messages = openai_util.update_chat(messages, "assistant", response)
+                st.session_state.past.append(query)
+                st.session_state.generated.append(response)
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
 
 # with st.expander("Show Messages"):
 #     st.write(st.session_state['messages'])
